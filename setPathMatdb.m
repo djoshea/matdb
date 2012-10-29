0ad8b93ea0fac416e94cdf0e541bed5faaca2f22
Dan O'Shea <dan@djoshea.com>
2012-10-28 22:35:22 -0700

    Adding setPathMatDb

---
+++ setPathMatdb.m
@@ -0,0 +1,6 @@
+function matdbSetPath()
+    % requires matlab-utils to be on the path already
+    matDbRoot = pathToThisFile(); 
+    fprintf('Path: Adding matdb at %s\n', matDbRoot);
+    addPathRecursive(matDbRoot);
+end
