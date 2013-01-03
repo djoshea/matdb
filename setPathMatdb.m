de1775394985ab3544cf708df473f1663b7045ca
Dan O'Shea <dan@djoshea.com>
2013-01-03 12:48:03 -0500

    Fixing setPathMatdb function name

--- setPathMatdb.m
+++ setPathMatdb.m
@@ -1,4 +1,4 @@
-function matdbSetPath()
+function setPathMatdb()
     % requires matlab-utils to be on the path already
     matDbRoot = pathToThisFile(); 
     fprintf('Path: Adding matdb at %s\n', matDbRoot);
